396acfe9d9bb6e6ec6fb741c1967e21f17a82886
Lele Chen <lelechen@fb.com>
2021-04-26 16:29:27 -0400

    add test

--- test.py
+++ test.py
@@ -10,16 +10,13 @@ def resize_image(im, max_size=768):
         print(f"Resize image to ({str(int(im.shape[1]*ratio))}, {str(int(im.shape[0]*ratio))}).")
         return cv2.resize(im, (0,0), fx=ratio, fy=ratio)
     return im
-fd = face_detector.FaceAlignmentDetector(
-    lmd_weights_path="./models/detector/FAN/2DFAN-4_keras.h5"# 2DFAN-4_keras.h5, 2DFAN-1_keras.h5
-)
+
 
 im = cv2.imread("images/test5.jpg")[..., ::-1]
 im = resize_image(im) # Resize image to prevent GPU OOM.
 h, w, _ = im.shape
 # plt.imshow(im)
 idet = IrisDetector()
-idet.set_detector(fd)
 eye_lms = idet.detect_iris(im)
 print (eye_lms.shape)
 # Display detection result
