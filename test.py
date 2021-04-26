be5ed1008fce07f698797b9ae5641bf02903ebe3
Lele Chen <lelechen@fb.com>
2021-04-26 19:55:29 -0400

    add test

--- test.py
+++ test.py
@@ -14,7 +14,17 @@ def resize_image(im, max_size=768):
         print(f"Resize image to ({str(int(im.shape[1]*ratio))}, {str(int(im.shape[0]*ratio))}).")
         return cv2.resize(im, (0,0), fx=ratio, fy=ratio)
     return im
+def rect_to_bb(rect):
+    # take a bounding predicted by dlib and convert it
+    # to the format (x, y, w, h) as we would normally do
+    # with OpenCV
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
 
+    # return a tuple of (x, y, w, h)
+    return (x, y, w, h)
 
 im = cv2.imread("images/test5.jpg")[..., ::-1]
 im = resize_image(im) # Resize image to prevent GPU OOM.
@@ -32,9 +42,14 @@ draw = idet.draw_pupil(draw, eye_lms[0][1,...]) # draw right eye
 
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 # detect faces in the grayscale image
-bboxes = detector(gray, 1)
+bbox = detector(gray, 1)[0]
+# (x, y, w, h) = rect_to_bb(bboxes[0])
 
-x0, y0, x1, y1, _ = bboxes[0].astype(np.int32)
+x0 = int(rect.left())
+x1 = int(rect.right())
+y0 = int(rect.top())
+y1 = int(rect.bottom())
+
 plt.subplot(1,2,1)
 plt.imshow(draw)
 plt.subplot(1,2,2)
