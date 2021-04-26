4da7cb09a71b7b56365a9c0e58a9abf4f645cefc
Lele Chen <lelechen@fb.com>
2021-04-26 16:07:37 -0400

    add test

---
+++ test.py
@@ -0,0 +1,27 @@
+import cv2
+from matplotlib import pyplot as plt
+
+import numpy as np 
+
+fd = face_detector.FaceAlignmentDetector(
+    lmd_weights_path="./models/detector/FAN/2DFAN-4_keras.h5"# 2DFAN-4_keras.h5, 2DFAN-1_keras.h5
+)
+
+im = cv2.imread("images/test5.jpg")[..., ::-1]
+im = resize_image(im) # Resize image to prevent GPU OOM.
+h, w, _ = im.shape
+# plt.imshow(im)
+idet = IrisDetector()
+idet.set_detector(fd)
+eye_lms = idet.detect_iris(im)
+# Display detection result
+plt.figure(figsize=(15,10))
+draw = idet.draw_pupil(im, eye_lms[0][0,...]) # draw left eye
+draw = idet.draw_pupil(draw, eye_lms[0][1,...]) # draw right eye
+bboxes = fd.detect_face(im, with_landmarks=False)
+x0, y0, x1, y1, _ = bboxes[0].astype(np.int32)
+plt.subplot(1,2,1)
+plt.imshow(draw)
+plt.subplot(1,2,2)
+plt.imshow(draw[x0:x1, y0:y1])
+plt.savefig('foo.png')
