20846a7058fdcb65c04b9b99241b2a0628d2cdb5
Lele Chen <lelechen@fb.com>
2021-04-26 20:10:59 -0400

    add test

--- models/detector/iris_detector.py
+++ models/detector/iris_detector.py
@@ -32,9 +32,8 @@ class IrisDetector():
         self.elg = KerasELG()
         self.elg.net.load_weights(self.path_elg_weights)
         
-    def set_detector(self, detector, predictor):
+    def set_detector(self, detector):
         self.detector = detector
-        self.predictor = predictor
         
     def detect_iris(self, im, landmarks=None):
         """
@@ -45,11 +44,12 @@ class IrisDetector():
         """
             
         # try:    
-        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        rect = self.detector(gray, 1)[0]
-        landmarks = self.predictor(gray, rect)
-        lm = shape_to_np(landmarks)
-        print (lm)
+        # gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        # rect = self.detector(gray, 1)[0]
+        # landmarks = self.predictor(gray, rect)
+        # lm = shape_to_np(landmarks)
+        # print (lm)
+        faces, landmarks = self.detector.detect_face(im, with_landmarks=True) 
 
         # except:
         #     raise NameError("Error occured during face detection. Maybe face detector has not been set.")
@@ -57,20 +57,20 @@ class IrisDetector():
         left_eye_idx = slice(36, 42)
         right_eye_idx = slice(42, 48)
         output_eye_landmarks = []
-        # for lm in landmarks:
-        left_eye_im, left_x0y0 = self.get_eye_roi(im, lm[left_eye_idx])
-        right_eye_im, right_x0y0 = self.get_eye_roi(im, lm[right_eye_idx])
-        inp_left = self.preprocess_eye_im(left_eye_im)
-        inp_right = self.preprocess_eye_im(right_eye_im)
-        
-        input_array = np.concatenate([inp_left, inp_right], axis=0)            
-        pred_left, pred_right = self.elg.net.predict(input_array)
-        
-        lms_left = self.elg._calculate_landmarks(pred_left, eye_roi=left_eye_im)
-        lms_right = self.elg._calculate_landmarks(pred_right, eye_roi=right_eye_im)
-        eye_landmarks = np.concatenate([lms_left, lms_right], axis=0)
-        eye_landmarks = eye_landmarks + np.array([left_x0y0, right_x0y0]).reshape(2,1,2)
-        output_eye_landmarks.append(eye_landmarks)
+        for lm in landmarks:
+            left_eye_im, left_x0y0 = self.get_eye_roi(im, lm[left_eye_idx])
+            right_eye_im, right_x0y0 = self.get_eye_roi(im, lm[right_eye_idx])
+            inp_left = self.preprocess_eye_im(left_eye_im)
+            inp_right = self.preprocess_eye_im(right_eye_im)
+            
+            input_array = np.concatenate([inp_left, inp_right], axis=0)            
+            pred_left, pred_right = self.elg.net.predict(input_array)
+            
+            lms_left = self.elg._calculate_landmarks(pred_left, eye_roi=left_eye_im)
+            lms_right = self.elg._calculate_landmarks(pred_right, eye_roi=right_eye_im)
+            eye_landmarks = np.concatenate([lms_left, lms_right], axis=0)
+            eye_landmarks = eye_landmarks + np.array([left_x0y0, right_x0y0]).reshape(2,1,2)
+            output_eye_landmarks.append(eye_landmarks)
         return output_eye_landmarks
     
     @staticmethod
--- test.py
+++ test.py
@@ -3,35 +3,28 @@ from matplotlib import pyplot as plt
 from models.detector import face_detector
 from models.detector.iris_detector import IrisDetector
 import dlib
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('/raid/celong/lele/github/idinvert_pytorch/utils/shape_predictor_68_face_landmarks.dat')
-
 import numpy as np 
+
+
+# detector = dlib.get_frontal_face_detector()
+# predictor = dlib.shape_predictor('/raid/celong/lele/github/idinvert_pytorch/utils/shape_predictor_68_face_landmarks.dat')
+fd = face_detector.FaceAlignmentDetector(
+    lmd_weights_path="./models/detector/FAN/2DFAN-4_keras.h5"# 2DFAN-4_keras.h5, 2DFAN-1_keras.h5
+)
+
 def resize_image(im, max_size=768):
     if np.max(im.shape) > max_size:
         ratio = max_size / np.max(im.shape)
         print(f"Resize image to ({str(int(im.shape[1]*ratio))}, {str(int(im.shape[0]*ratio))}).")
         return cv2.resize(im, (0,0), fx=ratio, fy=ratio)
     return im
-def rect_to_bb(rect):
-    # take a bounding predicted by dlib and convert it
-    # to the format (x, y, w, h) as we would normally do
-    # with OpenCV
-    x = rect.left()
-    y = rect.top()
-    w = rect.right() - x
-    h = rect.bottom() - y
-
-    # return a tuple of (x, y, w, h)
-    return (x, y, w, h)
 
 im = cv2.imread("images/test5.jpg")[..., ::-1]
 im = resize_image(im) # Resize image to prevent GPU OOM.
 h, w, _ = im.shape
 # plt.imshow(im)
 idet = IrisDetector()
-idet.set_detector(detector, predictor)
+idet.set_detector(fa)
 
 eye_lms = idet.detect_iris(im)
 # print (eye_lms.shape)
@@ -40,15 +33,18 @@ plt.figure(figsize=(15,10))
 draw = idet.draw_pupil(im, eye_lms[0][0,...]) # draw left eye
 draw = idet.draw_pupil(draw, eye_lms[0][1,...]) # draw right eye
 
-gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-# detect faces in the grayscale image
-bbox = detector(gray, 1)[0]
-# (x, y, w, h) = rect_to_bb(bboxes[0])
+bboxes = fd.detect_face(im, with_landmarks=False)
+x0, y0, x1, y1, _ = bboxes[0].astype(np.int32)
 
-x0 = int(bbox.left())
-x1 = int(bbox.right())
-y0 = int(bbox.top())
-y1 = int(bbox.bottom())
+
+# gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+# # detect faces in the grayscale image
+# bbox = detector(gray, 1)[0]
+
+# x0 = int(bbox.left())
+# x1 = int(bbox.right())
+# y0 = int(bbox.top())
+# y1 = int(bbox.bottom())
 
 plt.subplot(1,2,1)
 plt.imshow(draw)
