76ec2be9c3747d85532e939fbe3384d75982ec85
Lele Chen <lelechen@fb.com>
2021-04-26 20:46:29 -0400

    add test

--- models/detector/ELG/elg_keras.py
+++ models/detector/ELG/elg_keras.py
@@ -1,5 +1,5 @@
-from tensorflow.keras.layers import *
-from tensorflow.keras.models import Model
+from keras.layers import *
+from keras.models import Model
 import tensorflow as tf
 import numpy as np
 
