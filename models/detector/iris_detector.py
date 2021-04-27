3545567d703775330c89112233b231c73703fb33
Lele Chen <lelechen@fb.com>
2021-04-27 09:56:48 -0400

    add test

--- models/detector/iris_detector.py
+++ models/detector/iris_detector.py
@@ -139,8 +139,5 @@ class IrisDetector():
         draw = cv2.circle(draw, (pupil_center[0], pupil_center[1]), stroke, (255,255,0), -1)        
         draw = cv2.polylines(draw, [np.array(pnts_outerline).reshape(-1,1,2)], isClosed=True, color=(125,255,125), thickness=stroke//2)
         draw = cv2.polylines(draw, [np.array(pnts_innerline).reshape(-1,1,2)], isClosed=True, color=(125,125,255), thickness=stroke//2)
-        
-
-    
         return draw
         
--- test.py
+++ test.py
@@ -23,12 +23,11 @@ def resize_image(im, max_size=768):
 im = cv2.imread("images/test5.jpg")[..., ::-1]
 im = resize_image(im) # Resize image to prevent GPU OOM.
 h, w, _ = im.shape
-# plt.imshow(im)
 idet = IrisDetector()
 idet.set_detector(fa)
 
 eye_lms = idet.detect_iris(im)
-# print (eye_lms.shape)
+
 # Display detection result
 plt.figure(figsize=(15,10))
 draw = idet.draw_pupil(im, eye_lms[0][0,...]) # draw left eye
@@ -44,10 +43,6 @@ lms =   eye_lms[0][1,...].astype(np.int32)[:,::-1]
 cv2.fillConvexPoly(blank_image, lms[:8], (0,0,255))
 cv2.fillConvexPoly(blank_image, lms[8:16], (255,0,0))
 cv2.imwrite('gg.png', blank_image)
-draw = draw * 0.5 + blank_image * 0.5
-# bboxes = fd.detect_face(im, with_landmarks=False)
-# x0, y0, x1, y1, _ = bboxes[0].astype(np.int32)
-
 
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 # detect faces in the grayscale image
